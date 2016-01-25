ab93c9ec98f0a7e231fe2d442d8627ad2107a99b
hstrey <Helmut.Strey@gmail.com>
2016-01-25 15:40:33 -0500

    Initial Commit

---
+++ ECG_mock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ECG_mock
+//
+//  Created by Helmut Strey on 1/25/16.
+//  Copyright © 2016 ___STREYLAB___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
