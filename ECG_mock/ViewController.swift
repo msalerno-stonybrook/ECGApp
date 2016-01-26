221b4d1b3ae61f77ef85ac8f934530dc30152aa4
hstrey <Helmut.Strey@gmail.com>
2016-01-26 14:28:47 -0500

    starting version for ECG mockup.  Implemented table view with pull to refresh functionality and fill it with some fake ecg sensors.

--- ECG_mock/ViewController.swift
+++ ECG_mock/ViewController.swift
@@ -8,11 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView:UITableView!
+    
+    let BLEdevices = [
+        ("ECG Sensor 1","BA12-A456-799A-1111"),
+        ("ECG Sensor 2","BA12-A456-799A-1112"),
+        ("EMG Sensor 1","BA12-A456-799A-1113"),
+        ("EMG Sensor 2","BA12-A456-799A-1114") ]
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "handleRefresh:", forControlEvents: UIControlEvents.ValueChanged)
+        
+        return refreshControl
+    }()
 
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return BLEdevices.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        
+        let (BLEname, uuid) = BLEdevices[indexPath.row]
+        cell.textLabel?.text = BLEname
+        cell.detailTextLabel?.text = uuid
+        
+        let myImage = UIImage(named: "Cell_Icons")
+        cell.imageView?.image = myImage
+        
+        return cell
+    }
+    
+    func handleRefresh(refreshControl: UIRefreshControl) {
+        // Do some reloading of data and update the table view's data source
+        // Fetch more objects from a web service, for example...
+        
+        self.tableView.reloadData()
+        refreshControl.endRefreshing()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.addSubview(self.refreshControl)
     }
 
     override func didReceiveMemoryWarning() {
